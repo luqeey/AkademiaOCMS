be09723507944cdd38822855a28564d82ef69260
luqeey <115131091+luqeey@users.noreply.github.com>
2023-12-12 18:39:40 +0100

    post metoda je hotova a ostava ten dalsi plugin na nejake hooknutie

--- plugins/app/arrival/Routes.php
+++ plugins/app/arrival/Routes.php
@@ -2,12 +2,33 @@
 
 use App\Arrival\Models\Arrival;
 
-Route::get('api/kokoti/primitivovia', function () {
+Route::get('api/arrivals/users', function () {
     try {
         $data = Arrival::all();
 
         return response()->json($data);
     } catch (\Exception $e) {
-        return response()->json(['error' => $e -> getMessage()], 500);
+        return response()->json(['error' => $e->getMessage()], 500);
     }
 });
+
+Route::post('api/custom/arrival', function () {
+    try {
+        $data = request()->validate([
+            'name' => 'required|string|max:255',
+            'created_at' => 'required|date_format:Y-m-d H:i:s',
+        ]);
+
+        $arrival = new Arrival;
+        $arrival->setAttribute('name', $data['name']);
+        $arrival->setAttribute('created_at', $data['created_at']);
+        $arrival->save();
+
+        return response()->json(['message' => 'Arrival created']);
+    } catch (\Illuminate\Validation\ValidationException $e) {
+        return response()->json(['validation_errors' => $e->validator->errors()], 422);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+});
+
